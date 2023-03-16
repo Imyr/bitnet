2a4d6e92d9cc2ac2d3c9591d23b090daee7e8905
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2023-03-16 21:48:31 +0530

    Improved error handling, and others

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::process::exit;
 use clap::{Parser, Subcommand};
 use reqwest::{Client, Response};
+use html_escape::decode_html_entities as decode;
 
 /// CLI to manage internet access for students of BIT Mesra
 #[derive(Parser)]
@@ -20,18 +22,20 @@ async fn parser(res: Response) -> String {
     let val = res.text().await;
     match val {
         Ok(result) => {
-            let message = result.split_once("<message><![CDATA[").unwrap().1
+            let message = decode(result
+                                .split_once("<message><![CDATA[").unwrap().1
                                 .split_once("]]></message>").unwrap().0
-                                .to_string();
+                                ).to_string();
             return message
         }
         Err(error) => {
-            panic!("{}", error)
+            eprintln!("Result parsing failed: {}", error);
+            exit(1)
         }
     }
 }
 
-async fn login(client: Client, credentials: (String, String)) -> Response {
+async fn login(client: &Client, credentials: (&String, &String)) -> Response {
     let login_url = "http://172.16.1.1:8090/login.xml";
     let form = [("mode", "191"), ("username", &credentials.0[..]), ("password", &credentials.1[..])];
     let res = client.post(login_url).form(&form).send().await;
@@ -40,12 +44,13 @@ async fn login(client: Client, credentials: (String, String)) -> Response {
             return result
         }
         Err(error) => {
-            panic!("{}", error)
+            eprintln!("Login failed: {}", error);
+            exit(1)
         }
     }
 }
 
-async fn logout(client: Client) -> Response {
+async fn logout(client: &Client) -> Response {
     let logout_url = "http://172.16.1.1:8090/logout.xml";
     let form = [("mode", "193"), ("username", " ")];
     let res = client.post(logout_url).form(&form).send().await;
@@ -54,7 +59,8 @@ async fn logout(client: Client) -> Response {
             return result
         }
         Err(error) => {
-            panic!("{}", error)
+            eprintln!("Logout failed: {}", error);
+            exit(1)
         }
     }
 }
@@ -65,10 +71,10 @@ async fn main() {
     let client = Client::new();
     match cli.command {
         Commands::Login { username, password } => {
-            println!("{}", parser(login(client, (username, password)).await).await);
+            println!("{}", parser(login(&client, (&username, &password)).await).await.replace("{username}", &username));
         }
         Commands::Logout => {
-            println!("{}", parser(logout(client).await).await);
+            println!("{}", parser(logout(&client).await).await);
         }
     }   
 }
