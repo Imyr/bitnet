ff2e5a451da57b6e03940dd989bef11445360f92
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2024-10-02 18:08:39 +0530

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::process::exit;
 use clap::{Parser, Subcommand};
-use reqwest::blocking::{Client, Response};
 use html_escape::decode_html_entities as decode;
+use reqwest::blocking::{Client, Response};
+use std::process::exit;
 
 /// CLI to manage internet access for students of BIT Mesra
 #[derive(Parser)]
@@ -13,20 +13,26 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Login to network
-    Login {username: String, password: String},
+    Login { username: String, password: String },
     /// Logout from network
-    Logout
+    Logout,
 }
 
 fn parser(res: Response) -> String {
     let val = res.text();
     match val {
         Ok(result) => {
-            let message = decode(result
-                                .split_once("<message><![CDATA[").unwrap().1
-                                .split_once("]]></message>").unwrap().0
-                                ).to_string();
-            return message
+            let message = decode(
+                result
+                    .split_once("<message><![CDATA[")
+                    .unwrap()
+                    .1
+                    .split_once("]]></message>")
+                    .unwrap()
+                    .0,
+            )
+            .to_string();
+            return message;
         }
         Err(error) => {
             eprintln!("Result parsing failed: {}", error);
@@ -37,12 +43,14 @@ fn parser(res: Response) -> String {
 
 fn login(client: &Client, credentials: (&String, &String)) -> Response {
     let login_url = "http://172.16.1.1:8090/login.xml";
-    let form = [("mode", "191"), ("username", &credentials.0[..]), ("password", &credentials.1[..])];
+    let form = [
+        ("mode", "191"),
+        ("username", &credentials.0[..]),
+        ("password", &credentials.1[..]),
+    ];
     let res = client.post(login_url).form(&form).send();
     match res {
-        Ok(result) => {
-            return result
-        }
+        Ok(result) => return result,
         Err(error) => {
             eprintln!("Login failed: {}", error);
             exit(1)
@@ -55,9 +63,7 @@ fn logout(client: &Client) -> Response {
     let form = [("mode", "193"), ("username", " ")];
     let res = client.post(logout_url).form(&form).send();
     match res {
-        Ok(result) => {
-            return result
-        }
+        Ok(result) => return result,
         Err(error) => {
             eprintln!("Logout failed: {}", error);
             exit(1)
@@ -70,10 +76,13 @@ fn main() {
     let client = Client::new();
     match cli.command {
         Commands::Login { username, password } => {
-            println!("{}", parser(login(&client, (&username, &password))).replace("{username}", &username));
+            println!(
+                "{}",
+                parser(login(&client, (&username, &password))).replace("{username}", &username)
+            );
         }
         Commands::Logout => {
             println!("{}", parser(logout(&client)));
         }
-    }   
+    }
 }
